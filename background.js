c9713c3ee393e2ee48004cb70c95759f576967c9
Mitja Felicijan <mitja.felicijan@gmail.com>
2022-12-13 18:40:26 +0100

    Added style link to the extension

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 function downloadPage() {
-    let = filename = 'gpt-export.html';
+    let filename = 'gpt-export.html';
     let text = document.querySelector('main').innerHTML;
+    let styleLink = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/mitjafelicijan/gptchat-export-extension/style.css">';
 
     let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`${styleLink} ${text}`));
     element.setAttribute('download', filename);
     element.style.display = 'none';
     document.body.appendChild(element);
