bebdbf62a3075bb791aed2c2a5dd83bbdbfa18c0
Mitja Felicijan <mitja.felicijan@gmail.com>
2022-12-13 18:52:22 +0100

    Embedded styles into the extension

--- background.js
+++ background.js
@@ -1,10 +1,53 @@
 function downloadPage() {
+    let styles = `body {
+        background-color: #fff;
+        color: #000;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        line-height: 1.25em;
+        max-width: 700px;
+        margin: 0 auto;
+    }
+
+    body>div [class*="react-scroll-to-bottom"]>.flex>div:nth-child(odd) {
+        font-size: 200%;
+        margin-bottom: 30px;
+        font-weight: 600;
+    }
+
+    body button {
+        display: none;
+    }
+
+    body svg {
+        display: none;
+    }
+
+    body textarea {
+        display: none;
+    }
+
+    body pre {
+        border: 1px solid #ccc;
+        padding: 5px;
+    }
+
+    body code {
+        background-color: #eee;
+    }
+
+    body pre code {
+        border: 0;
+        background-color: transparent;
+    }
+    `;
+
     let filename = 'gpt-export.html';
     let text = document.querySelector('main').innerHTML;
     let styleLink = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/mitjafelicijan/gptchat-export-extension/style.css">';
 
     let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`${styleLink} ${text}`));
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`<style>${styles}</style> ${text}`));
     element.setAttribute('download', filename);
     element.style.display = 'none';
     document.body.appendChild(element);
